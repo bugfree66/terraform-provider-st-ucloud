4f512f5687a489b966405596cc63924bae5d04ea
eric <eric@MacBook-Pro.local>
2023-11-29 09:32:15 +0800

    1

--- ucloud/resource_cdn_domain_ssl_association.go
+++ ucloud/resource_cdn_domain_ssl_association.go
@@ -105,7 +105,6 @@ func (r *ucloudCdnDomainSslAssociationResource) Update(ctx context.Context, req 
 	if err != nil {
 		resp.Diagnostics.AddError("[API ERROR] Fail to Update CdnDomainSslAssociation", err.Error())
 	}
-	api.DeleteUnusedCertificate(r.client, model.SslCertificateName.ValueString())
 	resp.Diagnostics.Append(resp.State.Set(ctx, &model)...)
 }
 
@@ -119,5 +118,4 @@ func (r *ucloudCdnDomainSslAssociationResource) Delete(ctx context.Context, req 
 	if err != nil {
 		resp.Diagnostics.AddError("[API ERROR] Fail to Delete CdnDomainSslAssociation", err.Error())
 	}
-	api.DeleteUnusedCertificate(r.client, model.SslCertificateName.ValueString())
 }
